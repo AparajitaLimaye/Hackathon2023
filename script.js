54d01c920f7e91134a471d5ca4ae25399ecec5f0
AparajitaLimaye <70778675+AparajitaLimaye@users.noreply.github.com>
2023-11-04 20:56:25 +0000

    Finished Snowman Game

--- script.js
+++ script.js
@@ -9,11 +9,11 @@ const words = [
   'VUE'
 ];
 
-const wordToGuess = "";
-const guessedLetters = [];
-const guessedWord = "";
-const wrongGuesses = 0;
-const imageCount = 0;
+const maxWrongGuesses = 6;
+var wordToGuess = '';
+var guessedLetters = [];
+var wrongGuesses = 0;
+const imageCount = 1;
 
 function selectRandomWord() {
   return words[Math.floor(Math.random() * words.length)];
@@ -22,10 +22,8 @@ function selectRandomWord() {
 function initializedGame() {
   wordToGuess = selectRandomWord();
   guessedLetters = Array(wordToGuess.length).fill('_');
-  wrongGuesses = 0;
 
   updateWordDisplay();
-  updateMeltingSnowmanGraphic();
   const lettersContainer = document.querySelector('.letters');
   while (lettersContainer.firstChild) {
     lettersContainer.removeChild(lettersContainer.firstChild);
@@ -51,3 +49,54 @@ function updateWordDisplay() {
   const wordContainer = document.querySelector('.word');
   wordContainer.innerText = guessedLetters.join(' ');
 }
+
+function handleGuess(letter) {
+  // If the letter has already been guessed, do nothing
+  if (guessedLetters.includes(letter)) {
+    return;
+  }
+
+  // Add the letter to the list of guessed letters
+  guessedLetters.forEach((guessedLetter, index) => {
+    if (wordToGuess[index] === letter) {
+      guessedLetters[index] = letter;
+    }
+  });
+
+  // If the letter is not in the hidden word, increment the wrong guesses count and update the Melting Snowman graphic
+  if (!wordToGuess.includes(letter)) {
+    wrongGuesses++;
+  }
+
+  // Update the word display
+  updateWordDisplay();
+
+  // Check if the game has been won or lost
+  checkWinOrLose();
+}
+
+
+function checkWinOrLose() {
+  if (guessedLetters.join('') === wordToGuess) {
+    const messageContainer = document.querySelector('.message');
+    messageContainer.innerText = 'You win!';
+    const letterButtons = document.querySelectorAll('.letters button');
+    letterButtons.forEach(button => {
+      button.disabled = true;
+      button.removeEventListener('click', handleGuess);
+    });
+  } else if (wrongGuesses >= maxWrongGuesses) {
+    const messageContainer = document.querySelector('.message');
+    messageContainer.innerText = `You lose! The word was "${wordToGuess}".`;
+    const meltingSnowmanContainer = document.querySelector('.MeltingSnowman');
+    meltingSnowmanContainer.innerHTML = `<img src="images/gameover.png" alt="gameover">`;
+    const letterButtons = document.querySelectorAll('.letters button');
+    letterButtons.forEach(button => {
+      button.disabled = true;
+      button.removeEventListener('click', handleGuess);
+    });
+  }
+}
+
+// Initialize the game when the page loads
+window.addEventListener('load', initializedGame);
